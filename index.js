d512e521c1ab0301119754d85a2f650fa7058f51
Kevin Bastian <kjb5642@rit.edu>
2018-01-27 13:57:58 -0600

    Adding documentation and useful comments

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
+// Initialize all the libs
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+// Use public folder for the html
 app.use(express.static(__dirname + '/public'));
 
+// Setting up socket connection method
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
 }
 
+// Setting up listener connection
 io.on('connection', onConnection);
-
 http.listen(port, () => console.log('listening on port ' + port));
--- public/main.js
+++ public/main.js
@@ -1,17 +1,21 @@
 'use strict';
 
 (function() {
-
+  // Setting up succcet
   var socket = io();
+
+  // Setting up canvas
   var canvas = document.getElementsByClassName('whiteboard')[0];
   var colors = document.getElementsByClassName('color');
   var context = canvas.getContext('2d');
 
+  // Default ink color
   var current = {
     color: 'black'
   };
   var drawing = false;
 
+  // Setting up listeners
   canvas.addEventListener('mousedown', onMouseDown, false);
   canvas.addEventListener('mouseup', onMouseUp, false);
   canvas.addEventListener('mouseout', onMouseUp, false);
@@ -20,13 +24,21 @@
   for (var i = 0; i < colors.length; i++){
     colors[i].addEventListener('click', onColorUpdate, false);
   }
-
+  // Socket Listener 
   socket.on('drawing', onDrawingEvent);
 
   window.addEventListener('resize', onResize, false);
   onResize();
 
-
+  /**
+   * Draws a line in the canvas
+   * @param x0: Initial x coordinate
+   * @param y0: Initial y coordinate
+   * @param x1: Final x coordinate
+   * @param y1: Final y coordinate 
+   * @param color: Color of the line to be draw
+   * @param emit: Whether or not to emit a message to the socket (to only emmit local lines)
+   */
   function drawLine(x0, y0, x1, y1, color, emit){
     context.beginPath();
     context.moveTo(x0, y0);
@@ -48,19 +60,31 @@
       color: color
     });
   }
-
+  
+  /**
+   * On Mouse down event
+   * @param e: The even object containing the new coordinates
+   */
   function onMouseDown(e){
     drawing = true;
     current.x = e.clientX;
     current.y = e.clientY;
   }
 
+  /**
+   * On Mouse up event
+   * @param e: The even object containing the new coordinates
+   */
   function onMouseUp(e){
     if (!drawing) { return; }
     drawing = false;
     drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
   }
 
+  /**
+   * On Mouse move event
+   * @param e: The even object containing the new coordinates
+   */
   function onMouseMove(e){
     if (!drawing) { return; }
     drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
@@ -68,11 +92,20 @@
     current.y = e.clientY;
   }
 
+  /**
+   * On color change event. Gets the color from the css class
+   * @param e: The even object containing the target to look for the color
+   */
   function onColorUpdate(e){
     current.color = e.target.className.split(' ')[1];
   }
 
-  // limit the number of events per second
+  
+  /**
+   * Event that limit the number of events per second. Useful for onMouse move
+   * @param callback: Method to execute after the throttle was executed
+   * @param delay: Delay between each call
+   */
   function throttle(callback, delay) {
     var previousCall = new Date().getTime();
     return function() {
@@ -85,13 +118,19 @@
     };
   }
 
+  /**
+   * Main method called from socket listener to draw a line segment
+   * @param data: The data of the line to draw
+   */
   function onDrawingEvent(data){
     var w = canvas.width;
     var h = canvas.height;
     drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
   }
 
-  // make the canvas fill its parent
+  /**
+   * Make the canvas fill its parent
+   */
   function onResize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
