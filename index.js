8fbb7714a809f963ca2eb00e88338c5bd4f9e952
bahdah <bcs3409@rit.edu>
2018-01-28 01:38:33 -0500

    hi

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 /**
- * Initial server connection. This function gets executed 
+ * Initial server connection. This function gets executed
  * when the server gets activated for the first time
  * @param socket: The socket to comunicate with
  */
@@ -33,15 +33,57 @@ http.listen(port, () => console.log('listening on port ' + port));
 // Load drawings from Database
 io.on('connect', loadDrawings);
 
+
+
+//==============
+
+//the window reload function. you could of course do anything here
+function forceMidnightPageReload() {
+  var MongoClient = mongo.MongoClient;
+  var url = process.env.MONGODB_URI;
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("heroku_9x9gsclt"); // Select db
+    dbo.collection("mural").remove();
+      db.close();
+
+  });
+
+}
+
+//helper function to build up the desire time trigger
+function forceMidnightPageReloadGetTargetTime(hour,minute) {
+  var t = new Date();
+  t.setHours(hour);
+  t.setMinutes(minute);
+  t.setSeconds(0);
+  t.setMilliseconds(0);
+  return t;
+}
+
+//get your offset to wait value
+var timetarget = forceMidnightPageReloadGetTargetTime(1,45).getTime();
+var timenow =  new Date().getTime();
+var offsetmilliseconds = timetarget - timenow;
+
+//if it isn't midnight yet, set a timeout.
+if (offsetmilliseconds &gt;= 0){
+setTimeout(function(){forceMidnightPageReload();}, offsetmilliseconds);
+}
+
+//==============
+
+
 /**
  * Loads all entries in db to initialize the canvas with the drawings
  * @param socket: The socket to make calls to in the client
  */
 function loadDrawings(socket){
-  
+
   var MongoClient = mongo.MongoClient;
   var url = process.env.MONGODB_URI;
-  
+
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("heroku_9x9gsclt"); // Select db
@@ -53,9 +95,9 @@ function loadDrawings(socket){
       console.log(result);
       db.close();
     });
-    
+
   });
-  
+
 }
 
 /**
@@ -64,12 +106,12 @@ function loadDrawings(socket){
  * @param socket: The socket of the connection to communicate with the front end
  */
 function saveLineToDB(data, socket){
-  // Save new line to database 
-  
+  // Save new line to database
+
   var MongoClient = mongo.MongoClient;
 
     var url = process.env.MONGODB_URI;
-  
+
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
@@ -79,6 +121,5 @@ function saveLineToDB(data, socket){
       db.close();
     });
   });
-  
-}
 
+}
